4372ed86890274a9ed11a20a12a27bf44d84c84c
maxarthay <maxarthay@ufl.edu>
2026-03-26 14:22:04 -0400

    React init. with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="dashboard">
+      <h1>PiArray Dashboard</h1>
+    </div>
+  )
+}
+
+export default App
